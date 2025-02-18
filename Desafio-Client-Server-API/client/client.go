463283b276b6383b8d57d6ceb6577ffde1a372c4
Alano de Campos <alanodecampos@gmail.com>
2025-02-17 21:53:31 -0300

    Resolução desafio client/server api

--- Desafio-Client-Server-API/client/client.go
+++ Desafio-Client-Server-API/client/client.go
@@ -2,25 +2,74 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+type CotacaoDolarResponse struct {
+	Usdbrl struct {
+		Bid string `json:"bid"`
+	} `json:"USDBRL"`
+}
+
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Erro ao criar requisição: %v", err)
 	}
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	io.Copy(os.Stdout, res.Body)
 
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Erro ao fazer requisição: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Erro ao ler resposta: %v", err)
+	}
+
+	var cotacao CotacaoDolarResponse
+	err = json.Unmarshal(body, &cotacao)
+	if err != nil {
+		log.Fatalf("Erro ao decodificar JSON: %v", err)
+	}
+
+	err = saveCotacaoToFile(cotacao.Usdbrl.Bid)
+	if err != nil {
+		log.Fatalf("Erro ao salvar cotação no arquivo: %v", err)
+	}
+
+	fmt.Println("Cotação salva com sucesso em cotacao.txt")
+
+	select {
+	case <-ctx.Done():
+		log.Println("Timeout ao receber resposta do servidor")
+		return
+	default:
+	}
+
+}
+
+func saveCotacaoToFile(bid string) error {
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", bid))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- Desafio-Client-Server-API/server/server.go
+++ Desafio-Client-Server-API/server/server.go
@@ -3,14 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
 type CotacaoDolar struct {
-	Usdbrl struct {
+	USDBRL struct {
 		Code       string `json:"code"`
 		Codein     string `json:"codein"`
 		Name       string `json:"name"`
@@ -25,52 +27,105 @@ type CotacaoDolar struct {
 	} `json:"USDBRL"`
 }
 
+type CotacaoDolarModel struct {
+	gorm.Model
+	Bid string
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+
+	db, err := gorm.Open(sqlite.Open("cotacao.db?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate(&CotacaoDolarModel{})
+	if err != nil {
+		panic("failed to migrate database")
+	}
+
+	http.HandleFunc("/cotacao", handleCotacao(db))
+	log.Println("Servidor rodando na porta 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handleCotacao(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 
-	ctx := r.Context()
-	log.Println("Request iniciada")
-	defer log.Println("Request finalizada")
+		cotacao, err := getCotacao(ctx)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Erro ao obter cotação"))
+			log.Println("Erro ao obter cotação:", err)
+			return
+		}
 
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*200)
-	//ctx, cancel := context.WithCancel(ctx)
+		err = saveCotacao(ctx, db, cotacao.USDBRL.Bid)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Erro ao salvar cotação"))
+			log.Println("Erro ao salvar cotação:", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cotacao)
+	}
+}
+
+func getCotacao(ctx context.Context) (*CotacaoDolar, error) {
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	println(string(body))
 
-	var c CotacaoDolar
-	err = json.Unmarshal(body, &c)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	w.Write([]byte(body))
+	var cotacao CotacaoDolar
+	err = json.Unmarshal(body, &cotacao)
+	if err != nil {
+		return nil, err
+	}
 
-	//select {
-	//case <-time.After(time.Second * 5):
-	//	log.Println("Request processada com sucesso - Cmd")
-	//	w.Write([]byte(body))
-	//case <-ctx.Done():
-	//	log.Println("Request cancelada pelo cliente - Cmd")
-	//}
+	select {
+	case <-ctx.Done():
+		log.Println("Timeout ao obter cotação da API")
+		return nil, ctx.Err()
+	default:
+		return &cotacao, nil
+	}
+}
 
-	//req, err := http.NewRequestWithContext(ctx, "GET", "https://httpbin.org/get", nil)
+func saveCotacao(ctx context.Context, db *gorm.DB, bid string) error {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+	defer cancel()
 
+	cotacaoModel := CotacaoDolarModel{Bid: bid}
+
+	result := db.WithContext(ctx).Create(&cotacaoModel)
+	if result.Error != nil {
+		select {
+		case <-ctx.Done():
+			log.Println("Timeout ao salvar cotação no banco de dados")
+			return ctx.Err()
+		default:
+			log.Println("Erro ao salvar cotação no banco de dados:", result.Error) // Log the specific GORM error
+			return result.Error
+		}
+	}
+
+	return nil
 }
